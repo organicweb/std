c4988b0500532ddbbe8b83668c64e7a92fe06ebe
Mathias <contact@organicweb.fr>
2012-11-14 16:02:41 +0100

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Std::Application.config.secret_token = 'e1ba411c914433f6a65f5e8b6bbfae1526f6f038c7a735b48c6521489c06d0c260d07590e896f2aaee2b83620133a36ebeaebc83b84e0e3cdd8afc598b0a3d3d'
